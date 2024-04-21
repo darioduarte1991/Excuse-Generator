1f8a201213f53dbfa2a5e1017b1716d88bd83267
darioduarte1991 <162734794+darioduarte1991@users.noreply.github.com>
2024-04-21 15:51:00 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Using javascript, create a function that generates and returns a random excuse with the following structure:
+let who = ["El perro", "Mi abuela", "Mi tortuga", "Mi canario", "Mi gato"];
+let what = ["se comió", "aplastó", "robó", "rompió", "ensució"];
+let when = [
+  "antes de clases",
+  "justo cuando terminé",
+  "mientras almorzaba",
+  "mientras yo rezaba"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator(quien, que, cuando) {
+  numQuien = Math.floor(Math.random() * quien.length);
+  numQue = Math.floor(Math.random() * que.length);
+  numCuando = Math.floor(Math.random() * cuando.length);
+  return (
+    quien[numQuien] +
+    " " +
+    que[numQue] +
+    " " +
+    "mi tarea" +
+    " " +
+    cuando[numCuando]
+  );
+}
+function onLoad() {
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = excuseGenerator(who, what, when);
+}
+window.onload = onLoad;
